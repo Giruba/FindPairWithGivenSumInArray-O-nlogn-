6ff9b119e8c4fab4a1ef3bfcf6660bbed1c332f6
Giruba <gibeulah@microsoft.com>
2019-05-22 14:00:09 +0530

    Add project files.

---
+++ FindPariWithGivenSumInArray/InputProcessor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindPariWithGivenSumInArray
+{
+    class InputProcessor
+    {
+
+        public static int[] GetArrayFromInput() {
+
+            int[] array = null;
+
+            Console.WriteLine("Enter the number of elements in the array");
+            try
+            {
+                int numberOfElements = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the elements in the array separated by space, comma or semi-colon");
+                String[] elements = Console.ReadLine().Split(' ',',',';');
+                array = new int[numberOfElements];
+                for (int index = 0; index < numberOfElements; index++) {
+                    array[index] = int.Parse(elements[index]);
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("InputProcessor:GetArrayFromInput: Thrown exception is "+exception.Message);
+            }
+            return array;
+        }
+
+        public static void PrintPairsWithGivenSum(int[] array, int givenSum)
+        {
+
+            //Sort the array
+            Array.Sort(array);
+
+            int lowIndex = 0;
+            int highIndex = array.Length - 1;
+            while (lowIndex < highIndex)
+            {
+                if (array[lowIndex] + array[highIndex] == givenSum)
+                {
+                    Console.WriteLine("A pair with the given sum " + givenSum +
+                        " is found [" + array[lowIndex] + "," + array[highIndex] + "]");
+                    lowIndex++;
+                    highIndex--;
+                }
+                else if (array[lowIndex] + array[highIndex] < givenSum)
+                {
+                    lowIndex++;
+                }
+                else if (array[lowIndex] + array[highIndex] > givenSum)
+                {
+                    highIndex--;
+                }
+            }
+        }
+    }
+}
---
+++ FindPariWithGivenSumInArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindPariWithGivenSumInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find pair with given sum in an array!");
+            Console.WriteLine("-------------------------------------");
+
+            try {
+                int[] array = InputProcessor.GetArrayFromInput();
+                Console.WriteLine("Enter the sum to see if there are two elements in the array that " +
+                    "sum up to it");
+                int givenSum = int.Parse(Console.ReadLine());
+                InputProcessor.PrintPairsWithGivenSum(array, givenSum);
+            }catch(Exception exception){
+                Console.WriteLine("FindPariWithGivenSumInArray:Program: Thrown exception is "+exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
